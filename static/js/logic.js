99303d90d7851e629b7f23987e0e1c3737ff97ce
KevinW <kevinwan830803@gmail.com>
2023-08-17 17:15:52 -0400

    patch 1

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,15 +1,21 @@
 //Store the API
 
-let url = "./static/dataj/crimedata.json"
+let mapurl = "http://127.0.0.1:5000/map"
+let pieurl = "http://127.0.0.1:5000/pie"
+
 
 //perform a get request and load using d3
 
-d3.json(url).then(function(data){
+d3.json(mapurl).then(function(data){
+  
     
     //to check the data structure
     console.log(data)
+    console.log(data[0].properties.type)
+    //console.log(datap)
     
     createFeatures(data);
+ 
 });
 
 
@@ -19,51 +25,44 @@ function createFeatures(crimeData){
     //Define a funciton to run for each features.
     // Giving each feature a popup,
 
-    function onEachFeature(feature){
+    function onEachFeature(data, layer){
       
-      layer.bindPopup(`<h3>Crime was against:${feature.CrimeAgainst}' Offense Category:'${feature.OffenseCategory}</h3><hr><p>${new Date(feature.OccurDate)}</p> This Happening during ${feature.TimePeriod}`);
+      layer.bindPopup(`<h3>Crime was against:${data.properties.type}</h3>`);
     }
     
 // Create a GeoJson layer. Using On each and Point to Layer
-
-    let crimedata = L.geoJSON(crimeData,{
+// let markers = L.markerClusterGroup();
+    let crimes = L.geoJSON(crimeData,{
     onEachFeature: onEachFeature,
     pointToLayer: createMarker
+    // pointToLayer: markers.addLayer(createMarker)
+      
 });
-}
-
-// // Create GeoJson for plate
-// let plates = L.geoJSON(data,{
-//     style: function(){
-//         return{
-//             color: "black",
-//             weight:2.5
-//         }
-//     }
-// });
-
-//     createMap(earthquakes);
-// }
-
-//marker style
-
-function createMarker(feature, style){
-
-    return L.circle(style,{
-
-        radius :markersize(feature.properties.mag),
-        fillcolor:markerColor(feature.geometry.coordinates[2]),
-        color:markerColor(feature.geometry.coordinates[2]),
-        stroke: true,
-        weight: 3,
-        opacity: 0.5,
-        fillOpacity: 0.7
-
-    });
+    createMap(crimes);
+    // map.addLayer(markers);
 }
 
 
-function createMap(crime){
+//marker style 
+function createMarker(data, latlng){
+
+  return L.circle(latlng,{
+    radius :markersize(data.properties.total_offense),
+    fillcolor:"Red",
+    color:"Red",
+    stroke: true,
+    weight: 3,
+    opacity: 0.5,
+    fillOpacity: 0.7
+  });
+
+  
+}
+
+
+
+
+function createMap(crimes){
 
     // Create base layers.
     let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -83,67 +82,61 @@ function createMap(crime){
     
     // Crate overlay object to hold our overlay
     let overlayMaps = {
-    Earthquakes: crime 
-    
+    Crime: crimes 
     };
 
     // Create the map, giving it the streetmap and earthquake layers to display on default.
     let Map = L.map("map", {
     center: [
-      37.09, -95.71
+      45.5152, -122.6784
     ],
-    zoom: 5,
-    layers: [street, earthquakes]
+    zoom: 10,
+    layers: [street, crimes]
   });
 
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
+//   // Create a layer control.
+//   // Pass it our baseMaps and overlayMaps.
+//   // Add the layer control to the map.
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(Map);
 
-// Create a legend to display information about our map.
-    let info = L.control({
-        position: "bottomright"
-        });
-
-// When the layer control is added, insert a div with the class of "legend".
-    info.onAdd = function(Map) {
-        let div = L.DomUtil.create("div", "legend"),
-        labels = [],
-        catergory = ['Property','Person','Society'],
-        legendInfo = "<h1>Depth</h1>";
-
-
-    for (let i = 0; i < catergory.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + markerColor(catergory[i] + 1) + '"></i> ' +
-            catergory[i] + (catergory[i + 1] ? '&ndash;' + catergory[i + 1] + '<br>' : '+');
-    }   
-
-    return div;
-   
-  };
-
-  info.addTo(Map);
-
+ 
 }
+
+// // Create a legend to display information about our map.
+//     let info = L.control({
+//         position: "bottomright"
+//         });
+
+// // When the layer control is added, insert a div with the class of "legend".
+//     info.onAdd = function(Map) {
+//         let div = L.DomUtil.create("div", "legend"),
+//         labels = [],
+//         catergory = ['Property','Person','Society'],
+//         legendInfo = "<h1>Depth</h1>";
+
+
+//     for (let i = 0; i < catergory.length; i++) {
+//         div.innerHTML +=
+//             '<i style="background:' + markerColor(catergory[i] + 1) + '"></i> ' +
+//             catergory[i] + (catergory[i + 1] ? '&ndash;' + catergory[i + 1] + '<br>' : '+');
+//     }   
+
+//     return div;
+   
+//   };
 
 //Market size
 
 function markersize(totalnumber){
-    return totalcrime * 5;
+    return totalnumber * 1;
 }
 
 
 // //Adding
-// function markerColor(crimeagainst) {
-//     return  crimeagainst= "Property" ? '#1C0A00' :
-//             crimeagainst= "Society" ? '#8E3200' :
-//             crimeagainst= "Person" ? '#A64B2A' :
+// function markerColor(total) {
+//     return  total= "Property" ? '#1C0A00' :
+//             total= "Society" ? '#8E3200' :
+//             total= "Person" ? '#A64B2A' :
 // }
-
-
-  
-
