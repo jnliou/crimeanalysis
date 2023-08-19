4cb66e7b6144f9bea8ea868b55414cf25e837c5e
KevinW <kevinwan830803@gmail.com>
2023-08-19 18:56:47 -0400

    Added different maps

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,12 +3,20 @@ let mapurl = "http://127.0.0.1:5000/map"
 let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
-let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+let googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+  maxZoom: 20,
+  subdomains:['mt0','mt1','mt2','mt3']
 });
+
+let googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+  maxZoom: 20,
+  subdomains:['mt0','mt1','mt2','mt3']
+});
+
 let baseMaps = {
   "Street Map": street,
-  "Toto": topo
+  "Google Street": googleStreets,
+  "Satellite": googleSat
 };
 
 
@@ -55,7 +63,8 @@ function createFeatures(data){
     }
   });
 
-
+// Adding Layer controll on top right
+L.control.layers(baseMaps).addTo(map);
   
  
 // Create a legend to display information about our map.
